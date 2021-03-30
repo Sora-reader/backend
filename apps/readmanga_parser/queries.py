0fd4dc9356d8dea92009db596a95214129b6481b
ScriptHound <grinkiko@gmail.com>
2021-03-30 07:49:49 +0300

    Fixed api added api/ in urls.py at readmanga_parser deleted unnecessary method

--- apps/readmanga_parser/queries.py
+++ apps/readmanga_parser/queries.py
@@ -12,9 +12,5 @@ def get_or_none(cls, **kwargs) -> Union[Any, None]:
 
 class MangaQuery:
     @staticmethod
-    def get_manga_by_title(self, title: str) -> Union[Manga, None]:
+    def get_manga_by_title(title: str) -> Union[Manga, None]:
         return get_or_none(Manga, name__icontains=title)
-
-    # @staticmethod
-    # def get_manga_by_pk(self, _id: int) -> Union[Manga, None]:
-    #     return get_or_none(Manga, id__icontains=_id)
