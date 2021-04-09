0e11c424431c727ceac86ac50f96ae6c2cebcdec
Egor Ena <yahor.yena@innowise-group.com>
2021-04-09 15:35:07 +0300

    Remade jwt auth
    Implement sign up through django Viewset, add routers from simple jwt lib

--- apps/login/urls.py
+++ apps/login/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
+from django.urls import include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
-from .views import SignUpView
+from apps.login.views import ProfileSignupViewset
+
+app_name = 'auth'
+
+router = DefaultRouter()
+router.register('', viewset=ProfileSignupViewset, basename="sign_up")
 
 urlpatterns = [
-    path("sign-up/", SignUpView.as_view()),
-    path("auth/", SignUpView.as_view()),
-    path("refresh/", SignUpView.as_view()),
+    path("", include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- manga_reader/urls.py
+++ manga_reader/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 
+
 apipatterns = [
-    path("auth/", include("apps.login.urls")),
+    path("", include("apps.login.urls")),
     path("readmanga/", include("apps.readmanga_parser.urls")),
 ]
 
 urlpatterns = [
     path("", admin.site.urls),
-    path("api", include(apipatterns)),
+    path("api/", include(apipatterns)),
 ]
