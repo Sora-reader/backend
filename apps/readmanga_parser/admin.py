3f7dd5481741602f1ba382d9112f3139c7ede16f
Yury Lahodzich <yurylahodzich@gmail.com>
2021-05-24 21:56:29 +0300

    added django admin models configuration

--- apps/readmanga_parser/admin.py
+++ apps/readmanga_parser/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
 
-from . import models
+from apps.readmanga_parser.models import (
+    Translator,
+    Author,
+    Manga,
+    Genre,
+)
 
-admin.site.register(models.Manga)
-admin.site.register(models.Author)
-admin.site.register(models.Genre)
-admin.site.register(models.Translator)
+
+@admin.register(Manga)
+class MangaAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    list_display = ('name', 'status', 'year',)
+    list_filter = ('categories',)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    list_display = ('name',)
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    list_display = ('name',)
+
+
+@admin.register(Translator)
+class TranslatorAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    list_display = ('name',)
