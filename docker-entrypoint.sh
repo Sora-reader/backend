d5dc519389a13b76ab38f6cd31e89f0a9bd6dc80
dhvcc <1337kwiz@gmail.com>
2021-06-04 12:47:45 +0300

    Now docker uses global venv to install

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,7 +12,7 @@ if [ "$DEBUG" = 0 ]; then
     echo "Production mode"
     echo "==============="
     echo "Collecting static files to /app/staticfiles"
-    poetry run ./manage.py collectstatic --no-input --clear
+    ./manage.py collectstatic --no-input --clear
 else
     echo "Debug mode"
     echo "=========="
@@ -20,12 +20,12 @@ fi
 
 echo
 echo "Running migrations"
-poetry run ./manage.py migrate --no-input
+./manage.py migrate --no-input
 
 echo
 echo "Compiling messages"
-poetry run ./manage.py compilemessages -i venv
+./manage.py compilemessages -i venv
 
 echo "Running the server on port $PORT"
-poetry run celery -A manga_reader.celery.app worker -n manga@%h --loglevel=INFO -B &
-poetry run ./manage.py runserver 0.0.0.0:$PORT
+celery -A manga_reader.celery.app worker -n manga@%h --loglevel=INFO -B &
+./manage.py runserver 0.0.0.0:$PORT
