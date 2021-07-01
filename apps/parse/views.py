beacc72d7b0ee6f44006ada72355ac837340c729
NikDark <nikefremov.00.008@hotmail.com>
2021-07-01 18:25:12 +0300

    Updated API - Updated MangaChapterSerializer - Updated endpoints for details, chapters and images - Updated MangaSerializer - Add queryparam `parse` for endpoints

--- apps/parse/views.py
+++ apps/parse/views.py
@@ -1,6 +1,7 @@
 from django.db.models.query_utils import Q
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import OpenApiParameter, extend_schema
+from requests.exceptions import MissingSchema
 from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
@@ -8,6 +9,8 @@ from rest_framework.response import Response
 
 from apps.core.utils import init_redis_client
 from apps.parse.models import Chapter, Manga
+from apps.parse.readmanga.chapter_parser.parse import chapters_manga_info
+from apps.parse.readmanga.detail_parser.parse import deepen_manga_info
 from apps.parse.readmanga.images_parser.parse import parse_new_images
 from apps.parse.serializers import MangaChaptersSerializer, MangaSerializer
 
@@ -17,8 +20,11 @@ class MangaViewSet(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = Manga.objects.all()
     redis_client = init_redis_client()
 
-    def get_object(self, manga_id):
-        return get_object_or_404(Manga, id=manga_id)
+    def retrieve(self, request, pk, *args, **kwargs):
+        parse = request.GET.get("parse", None)
+        if parse is not None:
+            deepen_manga_info(pk)
+        return super().retrieve(request, *args, **kwargs)
 
     @extend_schema(
         parameters=[
@@ -31,8 +37,14 @@ class MangaViewSet(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
         url_path="(?P<manga_id>[^/.]+)/chapters",
     )
     def chapters_list(self, request, manga_id):
-        mangas = Manga.objects.filter(id=manga_id)
-        serializer = MangaChaptersSerializer(mangas, many=True)
+        manga: Manga = get_object_or_404(Manga, id=manga_id)
+        parse = request.GET.get("parse", None)
+        if parse is not None:
+            try:
+                chapters_manga_info(manga.pk)
+            except MissingSchema:
+                return Response("Parse the manga details", status=status.HTTP_400_BAD_REQUEST)
+        serializer = MangaChaptersSerializer(manga.volumes.all(), many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     @extend_schema(
