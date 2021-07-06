c0735e6494eb42d356977c591003dafa4ac8d71a
dhvcc <1337kwiz@gmail.com>
2021-07-07 01:53:34 +0300

    Fix on-demand image parsing
    fix #81

--- apps/parse/views.py
+++ apps/parse/views.py
@@ -46,7 +46,8 @@ class MangaViewSet(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     def images_list(self, request, chapter_id):
         chapter: Chapter = get_object_or_404(Chapter, id=chapter_id)
         parse = request.GET.get("parse", None)
-        if parse is not None:
+        images_cached = self.redis_client.exists(chapter.link)
+        if not images_cached or parse is not None:
             return Response(
                 parse_new_images(chapter.link, self.redis_client), status=status.HTTP_200_OK
             )
