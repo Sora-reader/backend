07a2a74b0ee37de1a377d3bd52a258b1a0b17fac
NikDark <nikefremov.00.008@hotmail.com>
2021-07-26 13:51:11 +0300

    - Make dict for the parsers - Move consts.py from exclude to the per-file-ignores in the flake8

--- apps/parse/parsers.py
+++ apps/parse/parsers.py
@@ -6,6 +6,7 @@ from scrapy.utils.project import get_project_settings
 
 from apps.parse.mangalib.detail_parser.parser import detail_manga_parser
 from apps.parse.mangalib.list_parser.parser import Crawler
+from apps.parse.models import Manga
 from apps.parse.readmanga.chapter_parser.parse import chapters_manga_info
 from apps.parse.readmanga.detail_parser.parse import deepen_manga_info
 from apps.parse.readmanga.images_parser.parse import images_manga_info
@@ -32,8 +33,20 @@ def mangalib_parser(settings=None, logger=None):
     asyncio.get_event_loop().run_until_complete(crawler.get_list())
 
 
-readmanga_detail_parse = deepen_manga_info
-readmanga_chapter_parse = chapters_manga_info
-readmanga_image_parse = images_manga_info
+DETAIL_PARSER = "details"
+LIST_PARSER = "list"
+IMAGE_PARSER = "images"
+CHAPTER_PARSER = "chapters"
 
-mangalib_detail_parse = detail_manga_parser
+PARSERS = {
+    Manga.SOURCE_MAP.get("https://readmanga.live"): {
+        DETAIL_PARSER: deepen_manga_info,
+        CHAPTER_PARSER: chapters_manga_info,
+        IMAGE_PARSER: images_manga_info,
+        LIST_PARSER: readmanga_parser,
+    },
+    Manga.SOURCE_MAP.get("https://mangalib.me"): {
+        DETAIL_PARSER: detail_manga_parser,
+        LIST_PARSER: mangalib_parser,
+    },
+}
