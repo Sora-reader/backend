380c4a5092ad78116c8fb21794d05b7250e2a0d0
dhvcc <1337kwiz@gmail.com>
2021-11-11 19:31:18 +0300

    Gunicorn

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -24,4 +24,5 @@ echo "Running migrations"
 
 echo "Running the server on port $PORT"
 
-./manage.py runserver_plus $PORT
+core_count=$(grep 'cpu[0-9]' /proc/stat | wc -l)
+gunicorn manga_reader.wsgi:application --bind 127.0.0.1:$PORT --workers $core_count*2+1
