9e46faba81f785123f186d8b55497d5b36ec1a6e
dhvcc <1337kwiz@gmail.com>
2022-04-28 20:44:52 +0300

    Remove transactions from sentry

--- manga_reader/settings.py
+++ manga_reader/settings.py
@@ -277,7 +277,6 @@ SENTRY_DSN = os.getenv("SENTRY_DSN", "")
 sentry_sdk.init(
     dsn=SENTRY_DSN,
     integrations=[DjangoIntegration()],
-    traces_sample_rate=1.0,
     send_default_pii=True,
 )
 ignore_logger("django.security.DisallowedHost")
